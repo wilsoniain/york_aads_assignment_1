af559fd9593e34b81895462a208ff5fc2a90baed
wilsoniain <wilsoniain@live.com>
2019-05-28 22:22:00 +0100

    commit

--- ModuleGrader_XXXX/src/ModuleGrader_XXXX.java
+++ ModuleGrader_XXXX/src/ModuleGrader_XXXX.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
+//York University Algorithms and Data Structures - Assignment 1
+
 public class ModuleGrader_XXXX {
+
+    int y;
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+        Scanner gradeScan = new Scanner(System.in);
+        int y = new gradeScan.nextInt();
+
+
+    }
+
+    public void gradeModule(y) {
+
+        if (y < 10) {
+            System.out.println("Too Low");
+        } else {
+            System.out.println("Too High");
+
+        }
+
+    }
+
+
 }
+
+
+
